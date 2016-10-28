b427dea64792b2317b561fcf5316f984d149e4d6
bubbleboy14 <mario.balibrera@gmail.com>
2016-10-28 15:34:10 -0700

    about page: fixed member names; updated old CT.dom.node() invocations to use div() instead

--- ctcoop/js/coop/pages/about.js
+++ ctcoop/js/coop/pages/about.js
@@ -6,27 +6,27 @@ CT.onload(function() {
 	var cfg = core.config.ctcoop.about;
 	CT.initCore();
 	user.core.all(function(users) {
-		var slider, content = [ CT.dom.node(cfg.header, "div", "biggest pv10") ];
+		var slider, content = [ CT.dom.div(cfg.header, "biggest pv10") ];
 		if (cfg.slider) {
-			slider = CT.dom.node(null, "div", "abs ctl top50 w3-10 h250p");
+			slider = CT.dom.div(null, "abs ctl top50 w3-10 h250p");
 			content = content.concat([
-				CT.dom.node(cfg.blurb, "div", "w2-3 h250p justified scrolly", "aboutblurb"),
+				CT.dom.div(cfg.blurb, "w2-3 h250p justified scrolly", "aboutblurb"),
 				slider
 			]);
 		} else
 			content.push(cfg.blurb);
 		content = content.concat([
-			CT.dom.node(cfg.members.header, "div", "bigger pv10"),
+			CT.dom.div(cfg.members.header, "bigger pv10"),
 			users.map(function(u) {
-				return CT.dom.node(CT.dom.node([
-					CT.dom.node(u.name, "div", "big"),
+				return CT.dom.div(CT.dom.div([
 					CT.dom.img(u.img, "right w1-3 ph10"),
+					CT.dom.div(u.firstName + " " + u.lastName.charAt(0), "big"),
 					u.blurb,
-				], "div", "h300p margined padded round bordered justified"),
-					"div", "w1-3 vtop inline-block");
+				], "h300p margined padded round bordered justified"),
+					"w1-3 vtop inline-block");
 			})
 		]);
-		CT.dom.setContent("ctmain", CT.dom.node(content, "div", "abs all10"));
+		CT.dom.setContent("ctmain", CT.dom.div(content, "abs all10"));
 		if (slider) {
 			(new CT.slider.Slider({
 				parent: slider,
