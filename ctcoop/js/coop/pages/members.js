4b1579448c770407e389ed4208be82b3dc428175
bubbleboy14 <mario.balibrera@gmail.com>
2017-09-02 19:38:47 -0700

    members page: edit override()

--- ctcoop/js/coop/pages/members.js
+++ ctcoop/js/coop/pages/members.js
@@ -1,8 +1,10 @@
 CT.require("CT.all");
 CT.require("core");
 CT.require("user.core");
+CT.require("edit.core");
 
 CT.onload(function() {
 	CT.initCore();
 	user.core.results(core.config.ctcoop.members);
+	edit.core.override();
 });
