5cd1801a675668d2b437092a533f4aaa15761c04
bubbleboy14 <mario.balibrera@gmail.com>
2019-10-06 17:59:10 -0700

    model: Timeslot, Task, Responsibility

--- ctcoop/model.py
+++ ctcoop/model.py
@@ -3,3 +3,18 @@ from ctuser.model import CTUser
 
 class Member(CTUser):
     roles = db.String(repeated=True)
+
+class Timeslot(db.TimeStampedBase):
+	schedule = db.String(choices=["once", "weekly", "daily", "exception"])
+	when = db.DateTime()
+	duration = db.Float() # hours
+
+class Task(db.TimeStampedBase):
+	timeslots = db.ForeignKey(kind=Timeslot, repeated=True)
+	name = db.String()
+	description = db.Text()
+
+class Responsibility(db.TimeStampedBase):
+	steward = db.ForeignKey() # CTUser, subclass (such as Member), or custom
+	task = db.ForeignKey(kind=Task)
+	timeslots = db.ForeignKey(kind=Timeslot, repeated=True)
