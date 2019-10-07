2d18ac594112a064b1fddb76ccab12f80d925efa
bubbleboy14 <mario.balibrera@gmail.com>
2019-10-07 14:38:45 -0700

    Cal: dateClick(), dayClick(), daily(), help(), and buttons

--- ctcoop/js/coop/cal.js
+++ ctcoop/js/coop/cal.js
@@ -2,22 +2,48 @@ coop.cal = {};
 
 coop.cal.Cal = CT.Class({
 	CLASSNAME: "coop.cal.Cal",
-	dayClick: function(node, cal, day, date, month, year) {
+	dateClick: function(node, cal, day, date, month, year) {
 		this.log(date, month, year);
-		(new CT.modal.Modal({
-			transition: "slide",
-			content: [
-				CT.cal.stamp(day, date, month, year),
-				"[add stuff here...]"
-			]
-		})).show();
+		CT.modal.modal([
+			CT.cal.stamp(day, date, month, year),
+			"[add stuff here...]"
+		]);
+	},
+	dayClick: function(day) {
+		this.log(day);
+		CT.modal.modal([
+			day,
+			"[add stuff here...]"
+		]);
+	},
+	daily: function() {
+
+	},
+	help: function() {
+		CT.modal.modal([
+			CT.dom.div("How does this work?", "biggest centered"),
+			CT.dom.div([
+				"Click the 'daily task' button to create a new daily task",
+				"Click a day name to set up a weekly task.",
+				"Click a date to set up a non-recurring task.",
+				"Click the task edit button to edit text and timeslots.",
+				"Click a task to volunteer."
+			], "subpadded")
+		]);
 	},
 	build: function() {
 		var cal = this.cal = new CT.cal.Cal({
-			dayClick: this.dayClick,
-			appointments: this.tasks
+			appointments: this.tasks,
+			click: {
+				date: this.dateClick,
+				day: this.dayClick
+			}
 		});
-		CT.dom.setContent(this.opts.parent, cal.node);
+		CT.dom.setContent(this.opts.parent, [
+			CT.dom.button("help", this.help, "right"),
+			CT.dom.button("daily task", this.daily, "left"),
+			cal.node
+		]);
 	},
 	load: function() {
 		var thiz = this;
