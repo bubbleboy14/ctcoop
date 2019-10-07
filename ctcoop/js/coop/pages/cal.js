456bffd5a1809b68473369f1fed5b84efdd6d326
bubbleboy14 <mario.balibrera@gmail.com>
2019-10-06 19:51:53 -0700

    cal page loader: use cal module

--- ctcoop/js/coop/pages/cal.js
+++ ctcoop/js/coop/pages/cal.js
@@ -1,12 +1,9 @@
 CT.require("CT.all");
 CT.require("core");
 CT.require("user.core");
+CT.require("coop.cal");
 
 CT.onload(function() {
 	CT.initCore();
-	CT.db.get("task", function(tasks) {
-		CT.dom.setContent("ctmain", (new CT.cal.Cal({
-			appointments: tasks
-		})).node);
-	});
+	new coop.cal.Cal();
 });
