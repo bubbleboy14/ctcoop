02b78f91183c73d2ac487a6dda143b2a4d41a8d8
bubbleboy14 <mario.balibrera@gmail.com>
2019-10-13 19:24:26 -0700

    model: replaced slot2steward() w/ Timeslot.slotter(); Timeslot.beforeremove() (if slotter, rms self from timeslots[]); Steward - beforeremove() (if task, rm self from commitments[]) and afterremove() (rm timeslots); Task.afterremove() (rm timeslots[] and commitments[])

--- ctcoop/model.py
+++ ctcoop/model.py
@@ -5,27 +5,45 @@ class Member(CTUser):
     roles = db.String(repeated=True)
 
 class Timeslot(db.TimeStampedBase):
-	schedule = db.String(choices=["once", "weekly",
-		"daily", "exception", "offday"])
-	when = db.DateTime()
-	duration = db.Float() # hours
+    schedule = db.String(choices=["once", "weekly", "daily", "exception", "offday"])
+    when = db.DateTime()
+    duration = db.Float() # hours
+
+    def slotter(self):
+        k = self.key.urlsafe()
+        return Stewardship.query(Stewardship.timeslots.contains(k)).get() or Task.query(Task.timeslots.contains(k)).get()
+
+    def beforeremove(self, session):
+        slotter = self.slotter()
+        if slotter: # no slotter if slotter is deleting itself....
+            slotter.timeslots = filter(lambda x : x != self.key, slotter.timeslots)
+            slotter.put(session)
 
 class Stewardship(db.TimeStampedBase):
-	steward = db.ForeignKey() # CTUser, subclass (such as Member), or custom
-	timeslots = db.ForeignKey(kind=Timeslot, repeated=True)
+    steward = db.ForeignKey() # CTUser, subclass (such as Member), or custom
+    timeslots = db.ForeignKey(kind=Timeslot, repeated=True)
 
-	def task(self):
-		return Task.query(Task.commitments.contains(self.key.urlsafe())).get()
+    def task(self):
+        return Task.query(Task.commitments.contains(self.key.urlsafe())).get()
 
-def slot2stewardship(slot):
-	return Stewardship.query(Stewardship.timeslots.contains(slot.key.urlsafe())).get()
+    def beforeremove(self, session):
+        task = self.task()
+        if task: # no task is task is deleting itself...
+            task.commitments = filter(lambda x : x != self.key, task.commitments)
+            task.put(session)
+
+    def afterremove(self, session):
+        db.delete_multi(db.get_multi(self.timeslots, session), session)
 
 class Task(db.TimeStampedBase):
-	editors = db.ForeignKey(repeated=True) # CTUser, subclass (such as Member), or custom
-	timeslots = db.ForeignKey(kind=Timeslot, repeated=True)
-	commitments = db.ForeignKey(kind=Stewardship, repeated=True)
-	name = db.String()
-	description = db.Text()
-	mode = db.String() # arbitrary
-	requirements = db.String(repeated=True)
-	steps = db.String(repeated=True)
+    editors = db.ForeignKey(repeated=True) # CTUser, subclass (such as Member), or custom
+    timeslots = db.ForeignKey(kind=Timeslot, repeated=True)
+    commitments = db.ForeignKey(kind=Stewardship, repeated=True)
+    name = db.String()
+    description = db.Text()
+    mode = db.String() # arbitrary
+    requirements = db.String(repeated=True)
+    steps = db.String(repeated=True)
+
+    def afterremove(self, session):
+       db.delete_multi(db.get_multi(self.timeslots + self.commitments, session), session)
