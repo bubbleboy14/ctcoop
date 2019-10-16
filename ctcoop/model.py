d9655b447edbb0f93e79c7294cc7e253e8da23f7
bubbleboy14 <mario.balibrera@gmail.com>
2019-10-15 20:46:09 -0700

    model: Timeslot - stewardship(), task(), beforeedit() (downschedule()s task as necessary); Stewardship.beforeremove() calls unsteward() if task is present (not self-deleting); RESCHED (should go somewhere else); Task - unsteward() (resched/remove steward notifier), downschedule() (called from Timeslot.beforeedit()), beforeremove() - unsteward()s all commitments[]

--- ctcoop/model.py
+++ ctcoop/model.py
@@ -1,4 +1,5 @@
-from cantools import db
+from cantools import db, config
+from cantools.web import send_mail
 from ctuser.model import CTUser
 
 class Member(CTUser):
@@ -9,9 +10,21 @@ class Timeslot(db.TimeStampedBase):
     when = db.DateTime()
     duration = db.Float() # hours
 
+    def stewardship(self):
+        return Stewardship.query(Stewardship.timeslots.contains(self.key.urlsafe())).get()
+
+    def task(self):
+        return Task.query(Task.timeslots.contains(self.key.urlsafe())).get()
+
     def slotter(self):
-        k = self.key.urlsafe()
-        return Stewardship.query(Stewardship.timeslots.contains(k)).get() or Task.query(Task.timeslots.contains(k)).get()
+        return self.task() or self.stewardship()
+
+    def beforeedit(self, edits):
+        sched = edits.get("sched")
+        if sched == "daily" or sched == "weekly" and self.schedule == "once":
+            return
+        task = self.task()
+        task and task.downschedule()
 
     def beforeremove(self, session):
         slotter = self.slotter()
@@ -29,11 +42,26 @@ class Stewardship(db.TimeStampedBase):
     def beforeremove(self, session):
         task = self.task()
         if task: # no task is task is deleting itself...
+            task.unsteward(self)
             task.commitments = filter(lambda x : x != self.key, task.commitments)
             task.put(session)
 
     def afterremove(self, session):
         db.delete_multi(db.get_multi(self.timeslots, session), session)
+
+
+# TODO: put this somewhere!!!
+RESCHED = """Hello!
+
+You volunteered for this task:
+
+%s
+
+The task has been %s, so your commitment record has been removed.
+
+Please click <a href='""" + config.web.protocol + """://""" + config.web.domain + """/coop/cal.html'>here</a> to review your calendar.
+
+That's it!"""
 
 class Task(db.TimeStampedBase):
     editors = db.ForeignKey(repeated=True) # CTUser, subclass (such as Member), or custom
@@ -44,6 +72,22 @@ class Task(db.TimeStampedBase):
     mode = db.String() # arbitrary
     requirements = db.String(repeated=True)
     steps = db.String(repeated=True)
+
+    def unsteward(self, stewardship, verb="rescheduled"): # just a notifier
+        send_mail(to=stewardship.steward.get().email,
+            subject="commitment update", body=RESCHED%(self.name, verb))
+
+    def downschedule(self):
+        stewz = db.get_multi(self.commitments)
+        for stew in stewz:
+            self.unsteward(stew, "rescheduled")
+        self.commitments = []
+        self.put()
+        db.delete_multi(stewz)
+
+    def beforeremove(self, session):
+        for stew in db.get_multi(self.commitments, session):
+            self.unsteward(stew, "removed")
 
     def afterremove(self, session):
        db.delete_multi(db.get_multi(self.timeslots + self.commitments, session), session)
