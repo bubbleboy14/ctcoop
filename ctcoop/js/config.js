263adcdd3cfa38e8eff8632bff1dbc9e12c3c666
bubbleboy14 <mario.balibrera@gmail.com>
2020-04-03 14:15:33 -0700

    config: + offerings{password} and needs{blurs{},reflections{},fnames[],carriers[]

--- ctcoop/js/config.js
+++ ctcoop/js/config.js
@@ -18,5 +18,34 @@
 	"members": {
 		"model": "member",
 		"filters": {}
+	},
+	"needs": {
+		"blurz": {
+			"phone": ["what's your phone number?", "phone # please"],
+			"email": ["what's your email address?", "email address please"],
+			"name": ["what do you go by?", "what is your name?"],
+			"address": ["where do you live?", "what is your address?"]
+		},
+		"reflections": {
+			"need": {
+				"closed": "someone is doing it!",
+				"follow": "thanks! we'll follow up :)",
+				"please": "please tell us what you need",
+				"prompt": "Please describe what you need assistance with",
+				"description": ["what do you need help with?", "how can we assist you?"]
+			},
+			"resource": {
+				"closed": "that's it!",
+				"follow": "thanks! you're the best!",
+				"please": "please describe the resource",
+				"prompt": "Please describe the resource that you would like to offer",
+				"description": ["what would you like to offer?", "please describe your offering"]
+			}
+		},
+		"fnames": ['name', 'phone', 'email', 'address'],
+		"carriers": ["at&t", "verizon", "tmobile", "sprint"]
+	},
+	"offerings": {
+		"password": null
 	}
 }
