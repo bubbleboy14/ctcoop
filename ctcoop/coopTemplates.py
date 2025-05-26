ac7fd15c0c4746673ed5027dfb053ae6e4eb2edd
bubbleboy14 <mario.balibrera@gmail.com>
2025-05-26 13:33:23 -0700

    coopTemplates -RESCHED

--- ctcoop/coopTemplates.py
+++ ctcoop/coopTemplates.py
@@ -1,17 +1,5 @@
 from cantools import config
 
-RESCHED = """Hello!
-
-You volunteered for this task:
-
-%s
-
-The task has been %s, so your commitment record has been removed.
-
-Please click <a href='""" + config.web.protocol + """://""" + config.web.domain + """/coop/cal.html'>here</a> to review your calendar.
-
-That's it!"""
-
 UPDATE = """Hello!
 
 Here is an update from %s:
